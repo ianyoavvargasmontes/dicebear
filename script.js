beae3e0579766ac3268cb441076c2d681a432619
ianyoavvargasmontes-dotcom <ianyoav.vargasmontes@aulavirtual.umar.mx>
2026-01-16 08:21:36 -0600

    SCRPT DESCARGA

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function generarAvatar() {
 
     document.getElementById("avatar").src = url;
 }
+
+function descargarAvatar() {
+    const avatar = document.getElementById("avatar");
+
+    if (!avatar.src) {
+        alert("Primero genera un avatar");
+        return;
+    }
+
+    const enlace = document.createElement("a");
+    enlace.href = avatar.src;
+    enlace.download = "avatar.png";
+    document.body.appendChild(enlace);
+    enlace.click();
+    document.body.removeChild(enlace);
+}
